618c6a4add257b84c0138c0c29a123358690e6b0
James D Stewart Jr <jamstewjr@gmail.com>
2017-02-20 21:45:38 -0800

    Enables server starting for homepage

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-var express = require('express')
+const express = require('express')
 
 const router = express()
 
 router.get('/', (request, response) => {
-  response.send('Hi James')
+  response.send('I should probably change this.')
 })
 
 router.listen(2017, () => {
-  console.log('The server is on in 2017')
+  console.log('The server is listening on 2017')
+})
+
+router.use( (request, response) => {
+  res.status(404).send("Sorry can't find that!")
 })
