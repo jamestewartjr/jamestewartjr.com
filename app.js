6d84c1a7cf4f83df6cc171c48ed468ab864353eb
James D Stewart Jr <jamstewjr@gmail.com>
2017-02-24 10:18:02 -0800

    Connects style sheet to page

--- app.js
+++ app.js
@@ -2,27 +2,19 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-
-
-app.get('/', (request, response) => {
-  response.render('landing')
-})
-
-
-
-
-
-
 app.use( (request, response) => {
   response.status(404).send('Sorry cannot find that!')
 })
 
 app.set('views', path.join(__dirname, 'views') )
 app.set('view engine', 'pug')
-
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('port', 2017)
 
 app.listen(app.get('port'), () => {
   console.log('The server is listening on 2017')
 })
-app.set(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (request, response) => {
+  response.render('landing')
+})
