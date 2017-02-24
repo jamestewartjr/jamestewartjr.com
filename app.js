f34119abf7f3733791a3dd64dcc4eb92f29d3252
James D Stewart Jr <jamstewjr@gmail.com>
2017-02-24 15:18:23 -0800

    WIP: typing animation currently broken. Landing page structure complete

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const 
+const logger = require('morgan')
+
 // app.use( (request, response) => {
 //   response.status(404).send('Sorry cannot find that!')
 // })
 
 app.set('views', path.join(__dirname, 'views') )
 app.set('view engine', 'pug')
-app.use(express.static(path.join(__dirname, 'public')))
 app.set('port', 2017)
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger('dev'))
 
 app.listen(app.get('port'), () => {
   console.log('The server is listening on 2017')
