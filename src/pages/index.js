d93b0a1ad14ca5bcc3167c98614f96f3cbc14a0e
James Stewart <jamestewartjr1@gmail.com>
2019-06-03 06:48:05 -0400

    remove demo screenshots

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { graphql, withPrefix, Link } from 'gatsby';
 import Helmet from 'react-helmet';
 import SEO from '../components/SEO';
 import Layout from '../layouts/index';
-import Call from '../components/Call';
+// import Call from '../components/Call';
 
 const Home = (props) => {
   const markdown = props.data.allMarkdownRemark.edges;
