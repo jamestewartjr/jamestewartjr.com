4bd4940f2cccfc388fb8a3e0d315e2d30f82d120
James Stewart <jamestewartjr1@gmail.com>
2020-03-05 22:47:55 -0500

    convert UserLinks component to material-ui

--- src/components/UserLinks/index.jsx
+++ src/components/UserLinks/index.jsx
@@ -1,26 +1,24 @@
 import React, { Component } from "react";
-import Button from "react-md/lib/Buttons";
+import { Button } from '@material-ui/core';
 import "./UserLinks.scss";
 
 class UserLinks extends Component {
   getLinkElements() {
-    const { userLinks } = this.props.config;
-    const { labeled } = this.props;
+    const { labeled, config: { userLinks } } = this.props;
     return userLinks.map(link => (
       <Button
-        icon={!labeled}
-        flat={labeled}
-        secondary
+        color="secondary"
         key={link.label}
-        iconClassName={link.iconClassName}
-        href={link.url}
+        className={link.iconClassName}
+        href={`${link.url}`}
       >
         {labeled ? link.label : ""}
       </Button>
     ));
   }
+
   render() {
-    const { userLinks } = this.props.config;
+    const { config: { userLinks } } = this.props;
     if (!userLinks) {
       return null;
     }
