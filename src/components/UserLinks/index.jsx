86cc93a4b69043dca873a0c209d5d1abe9be8156
James Stewart <jamestewartjr1@gmail.com>
2020-03-08 21:56:14 -0400

    Convert Navvigation list icons to react-icons

--- src/components/UserLinks/index.jsx
+++ src/components/UserLinks/index.jsx
@@ -8,19 +8,6 @@ class UserLinks extends Component {
   getLinkElements() {
     const { labeled} = this.props;
     return (
-    // userLinks.map(link => (
-      // <Button
-      //   color="secondary"
-      //   key={link.label}
-      //   className={link.iconClassName}
-      //   href={`${link.url}`}
-      // >
-      //   {labeled ? link.label : ""}
-      // </Button>
-      // )
-      // <IconButton key={link.label} aria-label={link.label}  href={`${link.url}`}>
-      //   <link.iconClassName />
-      // </IconButton>
       <ul className="horizontal-links">
         {SOCIAL.map(s => (
           <li key={s.kind}>
@@ -32,8 +19,6 @@ class UserLinks extends Component {
           </li>
           ))}
       </ul>
-      
-    // )
     );
   }
 
