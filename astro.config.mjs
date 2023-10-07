77c643d40b315f4f8a08537a5829a216add02e57
James D Stewart <jamestewartjr1@gmail.com>
2023-10-07 12:24:19 -0400

    Add links page

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,10 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://jamestewartjr.com',
 	integrations: [mdx(), sitemap()],
+	pages: {
+		'links.astro': {
+		  title: "James Stewart Jr's Linktree",
+		  description: "A collection of links to James Stewart Jr's online presence.",
+		},
+	  },
 });
