6eea0ff08c270a88b4ebd9ada58ff5aaa19798d5
James D Stewart <jamestewartjr1@gmail.com>
2023-10-16 02:01:42 -0400

    remove useless file, update sitemap config, & add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jamestewartjr.com',
+	site: 'https://www.jamestewartjr.com',
 	integrations: [mdx(), sitemap()],
 	pages: {
 		'links.astro': {
