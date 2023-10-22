2ca89382b4a03bc85255bd1da0d55451d2dda3be
James D Stewart <jamestewartjr1@gmail.com>
2023-10-22 15:30:48 -0400

    include GA4 config info

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.jamestewartjr.com',
-	integrations: [mdx(), sitemap()],
-	pages: {
-		'links.astro': {
-		  title: "James Stewart Jr's Website",
-		  description: "James Stewart Jr's online presence.",
-		},
-	  },
+  site: 'https://www.jamestewartjr.com',
+  integrations: [mdx(), sitemap(), react()],
+  pages: {
+    'links.astro': {
+      title: "James Stewart Jr's Website",
+      description: "James Stewart Jr's online presence."
+    }
+  }
 });
