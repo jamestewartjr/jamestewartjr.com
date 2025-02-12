d10c077f2bba5f68f27c45d62014e9c19d76967e
James D Stewart <jamestewartjr1@gmail.com>
2025-02-11 21:03:14 -0500

    fix build issues

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,9 @@
-// import { BlogPosts } from 'app/components/posts'
+export const metadata = {
+	title: 'About',
+	description: 'About James Stewart Jr',
+}
 
-// export const metadata = {
-//   title: 'Blog',
-//   description: 'Read my blog.',
-// }
-
-export default function Page() {
+export default function About() {
   return (
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Current Projects</h1>
