46d7c5b9497b188974ce5bdf9211038ccd48cabc
James D Stewart <jamestewartjr1@gmail.com>
2025-03-15 23:55:02 -0400

    fix logging

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ... other config options
+  
+  // Disable Vercel Speed Insights
+  speedInsights: {
+    enabled: false,
+  },
+}
+
+module.exports = nextConfig 
