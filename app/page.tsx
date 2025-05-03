c54f3ed2c389d587aa7a2fb8c5395a44549d9e36
James D Stewart <jamestewartjr1@gmail.com>
2025-05-03 01:51:00 -0400

    update homepage bio and subtitle

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,12 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Welcome!
       </h1>
+      <h3><i>Frontend engineer building modular, user-first AI tools.</i></h3>
+      <br/>
       <p className="mb-4">
-        {`	I'm a software engineer, dad, and business owner. This is my part of the web.
-          Currently, I spend most of time with my family and my business.`}
+      I’m a frontend engineer transitioning into AI-focused UI development. 
+      I enjoy building clean, reusable components and creating interfaces that make working with AI more practical and approachable. 
+      Lately, I’ve been focused on prompt engineering and designing tools that help make complex workflows easier to manage.
       </p>
       <SocialLinks />
       <div className="my-8">
